2e0b75626145e624ffe5283b5e15799e49b97a17
aurette2 <charmeyebad@gmail.com>
2024-09-04 16:03:18 +0000

    Write and Run a Simple Unit Test

--- loadData.py
+++ loadData.py
@@ -1,9 +1,8 @@
 import pandas as pd
 from sklearn.datasets import load_iris
 
-def load_and_display_iris():
+def load_and_display_iris(data):
     # Load the Iris dataset
-    data = load_iris()
     data_df = pd.DataFrame(data=data.data, columns=data.feature_names)
     data_df['target'] = data.target
 
@@ -12,5 +11,7 @@ def load_and_display_iris():
     return data_df
 
 if __name__ == "__main__":
-    load_and_display_iris()
+
+    data = load_iris()
+    load_and_display_iris(data)
 
