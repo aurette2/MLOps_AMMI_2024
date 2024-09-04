a9f13e9224c0321aff41a5315376e17d6d19fa49
aurette2 <charmeyebad@gmail.com>
2024-09-04 16:03:18 +0000

    Write and Run a Simple Unit Test

--- train.py
+++ train.py
@@ -5,9 +5,7 @@ from sklearn.metrics import accuracy_score
 from loadData import load_and_display_iris
 
 #Split data into train and test sets
-def split():
-    # get data from load file
-    data_df = load_and_display_iris()
+def split(data_df):
     X = data_df.drop('target', axis=1)
     y = data_df['target']
 
@@ -15,24 +13,27 @@ def split():
     return X_train, X_test, y_train, y_test
 
 # Initialize and train the Logistic Regression model
-def fit(X_train, y_train):
+def fit(model, X_train, y_train):
     model.fit(X_train, y_train)
 
 # Make predictions on the test set
-def predict(Xtest):
+def predict(model, X_test):
     y_pred = model.predict(X_test)
     return y_pred
 
     
 
 if __name__ == "__main__":
+    # get data from load file
+    data_df = load_and_display_iris()
 
-    X_train, X_test, y_train, y_test = split()
+    #Splitting the dataset
+    X_train, X_test, y_train, y_test = split(data_df)
 
-
+    #Initialisation, training and evaluating the model
     model = LogisticRegression(max_iter=200)
-    fit(X_train, y_train)
-    y_pred = predict(X_test)
+    fit(model, X_train, y_train)
+    y_pred = predict(model, X_test)
 
     # Evaluate the model's accuracy
     accuracy = accuracy_score(y_test, y_pred)
