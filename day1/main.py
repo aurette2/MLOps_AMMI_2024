d820a576285b6a112bb569226dae2cc510fcd65f
aurette2 <charmeyebad@gmail.com>
2024-09-09 14:16:27 +0000

    day 2 task

---
+++ day1/main.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
+from day1.loadData import load_and_display_iris
+from day1.train import split, fit, predict
+from day1.visualizations import visualize_dataset, visualize_model_performance
+
+
+data = load_iris()
+data_df = load_and_display_iris(data)
+visualize_dataset(data_df)
+X_train, X_test, y_train, y_test= split(data_df)
+model = LogisticRegression(max_iter=200)
+fit(model, X_train, y_train)
+y_pred = predict(model, X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy of the Logistic Regression model: {accuracy:.2f}")
+visualize_model_performance(y_test, y_pred)
